584ff7167f10173ff908c32a077aff36fbcbbad2
Arunabh Das <arunabhdas@gmail.com>
2018-01-19 16:10:31 -0500

    Added collectionView

--- magicbullet-layoutkit/MagicBullet/MagicBullet/Controller/ThirdViewController.swift
+++ magicbullet-layoutkit/MagicBullet/MagicBullet/Controller/ThirdViewController.swift
@@ -7,23 +7,36 @@ import UIKit
 import YogaKit
 
 
-class ThirdViewController: UIViewController {
-
+class ThirdViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let nickProfile = MiniProfileLayout(
-            imageName: "bernie.jpg",
-            name: "Bernie Sanders",
-            headline: "President, White House"
-        )
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 90, height: 120)
         
-        nickProfile.arrangement(origin: CGPoint(x: 0, y: 100), width: self.view.frame.width).makeViews(in: self.view, direction: UIUserInterfaceLayoutDirection.leftToRight)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.backgroundColor = UIColor.white
+        self.view.addSubview(collectionView)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 14
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = UIColor.orange
+        return cell
     }
 
 }
