696db92b0150736cea419b7aad6e441591c4118d
Arunabh Das <arunabhdas@gmail.com>
2018-01-19 14:37:56 -0500

    Added MagicBullet using LayoutKit

---
+++ magicbullet-layoutkit/MagicBullet/Pods/Target Support Files/Pods-MagicBullet/Pods-MagicBullet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MagicBullet : NSObject
+@end
+@implementation PodsDummy_Pods_MagicBullet
+@end
---
+++ magicbullet-layoutkit/MagicBullet/Pods/Target Support Files/Pods-MagicBulletUITests/Pods-MagicBulletUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MagicBulletUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MagicBulletUITests
+@end
